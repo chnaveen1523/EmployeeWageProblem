2cbaceae714a49e3ac5f4b1638f85021c7321387
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 20:42:07 +0530

    [Naveen] Added a Month code

--- Program.cs
+++ Program.cs
@@ -9,37 +9,42 @@ namespace EmployeeWageCompute_CS
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            //UC4 Daily Employee Wage Calculation using switch
+            //UC5 Daily Employee Wage Calculation for a month
 
             //local variables
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-            //Predefined random class for generating random values
-            Random random = new Random();
-
-            //Next method() 0-initial value,3-number of element from 0
-            int empCheck = random.Next(0, 3);
-            Console.WriteLine("Checking employee status" + empCheck);
-
-            //Selection Switch statement
-            switch (empCheck)
+            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 4;
-                    break;
-            }
+                //Predefined random class for generating random values
+                Random random = new Random();
 
-            empWage = EMP_RATE_PER_HOUR * empHrs;
-            Console.WriteLine("Daily employee wage is:" + empWage);
+                //Next method() 0-initial value,3-number of element from 0
+                int empCheck = random.Next(0, 3);
+
+                //Selection Switch statement
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 4;
+                        break;
+                }
+
+                empWage = EMP_RATE_PER_HOUR * empHrs;
+                totalEmpWage += empWage;
+            }
+            Console.WriteLine("Total employee wage:" + totalEmpWage);
             Console.ReadLine();
         }
     }
